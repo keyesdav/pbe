0ee1d57d05419db6b1245bc87ff3b2c318c5b6a3
keyesdav <keyesdav@gmail.com>
2016-01-09 05:18:53 +0000

    start at practice, added count of selected questions

--- htdocs/client/js/app.js
+++ htdocs/client/js/app.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var app = angular.module('PbeApp', ['ui.router', 'ngMaterial', 'ngMdIcons', 'pbeServices', 'sticky']);
+  var app = angular.module('PbeApp', ['ui.router', 'ngMaterial', 'ngMdIcons', 'pbeServices', 'sticky', 'chart.js']);
 
   app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
@@ -28,6 +28,11 @@
         controller: 'PracticeController'
       })
 
+      .state('practice-progress', {
+        url: '/practice/progress',
+        templateUrl: 'partials/practice-progress.html',
+        controller: 'PracticeController'
+      })
   }]);
 
   app.controller('MainAppController', ['$scope', '$state', '$mdBottomSheet', '$mdSidenav', '$mdDialog', '$mdComponentRegistry', 'PbeTests', function($scope, $state, $mdBottomSheet, $mdSidenav, $mdDialog, $mdComponentRegistry, PbeTests) {
@@ -58,7 +63,7 @@
       title: 'Quiz Me!',
       icon: 'school'
     }, {
-      link: 'practiceStats',
+      link: 'practiceProgress',
       title: 'Quiz Stats',
       icon: 'timeline'
     }];
@@ -130,6 +135,11 @@
       $state.go('practice-select', {
       });
 
+    }
+    
+    $scope.practiceProgress = function($event){
+      $mdSidenav('left').toggle();
+      $state.go('practice-progress');
     }
 
   }]);
@@ -832,6 +842,14 @@
     $scope.selected = {};
     $scope.selected.bible=[];
     $scope.selected.commentary=[];
+    
+    // graph
+    $scope.labels = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40"];
+    $scope.series = ['Correct'];
+    $scope.data = [
+      [5,2,1,0,3,2,2,5,3,4,5,4,3,5,4,5,1,3,2,1,0,1,0,2,0,0,0,0,0,1,2,1,3,4,0,5,0,0,2,2],
+    ];
+
 
     var chapPromise = PbeService.getQuestionChapters("Exodus").$promise;
     chapPromise
